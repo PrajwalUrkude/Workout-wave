c2dfd95de555a5f3fdf203f05827f92d37ed0f09
Prajwal Urkude <prajwalurkude7767@gmail.com>
2024-07-24 16:07:16 +0530

    first commit

---
+++ Script.js
@@ -0,0 +1,6 @@
+// Navbar toggle button functionality:
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('mobile-menu').addEventListener('click', function() {
+        document.getElementById('nav-list').classList.toggle('show');
+    });
+});
